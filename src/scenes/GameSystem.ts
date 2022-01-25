cd5393d00d44de346d928ff7332b219a9b7bc053
hk-p114514 <hukada.gogotea@gmail.com>
2022-01-25 22:26:00 +0900

    マップの管理を考察

--- src/scenes/GameSystem.ts
+++ src/scenes/GameSystem.ts
@@ -1,7 +1,39 @@
-import { Types } from 'phaser';
+import { Scene, Types } from 'phaser';
 
-export type Mode = 'map' | 'battle' | 'menu';
+// 各モードの名前
+export type ModeName = 'map' | 'battle' | 'menu';
 
-class GameSystem {
-  private state: Mode = 'map';
+// モードを管理するシーン
+export type Mode = {
+  key: ModeName;
+  controller: Scene;
+};
+
+export class GameSystem {
+  // 現在のモード
+  private current: Mode;
+
+  // 各モードを管理するシーン
+  private modeControllers: Mode[];
+
+  // 十字キーの状態
+  private key?: Types.Input.Keyboard.CursorKeys;
+
+  constructor(controllers: Mode[], initialMode: Mode = controllers[0]) {
+    this.modeControllers = controllers;
+    this.current = initialMode;
+  }
+
+  // 現在のモードを変更する
+  public switchMode(name: ModeName): boolean {
+    const mode: Mode | undefined = this.modeControllers.find((mode) => mode.key === name);
+
+    if (mode) {
+      this.current = mode;
+    }
+
+    return !!mode;
+  }
+
+  update(): void {}
 }
