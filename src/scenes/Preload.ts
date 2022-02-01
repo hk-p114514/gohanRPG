2208dd810858bd6a20775535c78775c14a82b10a
hk-p114514 <hukada.gogotea@gmail.com>
2022-02-01 23:20:57 +0900

    tmxファイルを追加

--- src/scenes/Preload.ts
+++ src/scenes/Preload.ts
@@ -5,10 +5,10 @@ import { keys } from './Test2';
 
 // マップの画像とjsonファイルを読み込む
 import mapTiles from '@/assets/maps/map001.png';
-import mapJson from '@/json/map002.json';
 import Atlas from '@/assets/characters/dynamic/atlas.png';
 import AtlasJson from '@/json/atlas.json';
 import player from '@/assets/characters/dynamic/player.png';
+import onigiri from '@/assets/maps/onigiri.png';
 
 const width = W();
 const height = H();
@@ -28,11 +28,16 @@ class Preload extends Scene {
   init = () => {};
 
   preload = () => {
+    // 画像を読み込む
+    this.load.image('onigiri', onigiri);
+
+    // マップの画像とjsonファイルを読み込む
     this.load.image(keys.image, mapTiles);
-    this.load.tilemapTiledJSON(keys.json, mapJson);
+
     this.load.atlas('atlas', Atlas, AtlasJson);
 
     this.load.image('mapTiles', mapTiles);
+
     this.load.spritesheet('player', player, {
       frameWidth: characterSize,
       frameHeight: characterSize,
--- src/scenes/Test3.ts
+++ src/scenes/Test3.ts
@@ -1,11 +1,6 @@
+import { keys } from './Test2';
 import { Scene, Tilemaps, Game, Cameras, Input, GameObjects, Types } from 'phaser';
-import mapJson from '@/json/map002.json';
-
-export const keys = {
-  json: 'mapJson',
-  image: 'mapTiles',
-  player: 'player',
-};
+import mapJson3 from '@/json/map003.json';
 
 class Test2 extends Scene {
   private tileset?: Tilemaps.Tileset;
@@ -16,21 +11,23 @@ class Test2 extends Scene {
 
   constructor() {
     super({
-      key: 'Test2',
+      key: 'Test3',
     });
   }
 
   preload = () => {
-    this.load.tilemapTiledJSON(keys.json, mapJson);
+    console.log('preload');
+
+    this.load.tilemapTiledJSON('mapJson3', mapJson3);
   };
 
   create = () => {
     // enterキーでシーンを切り替え
     const enter = this.input.keyboard.addKey('ENTER');
     enter.on('down', () => {
-      this.scene.start('Test3');
+      // this.scene.start('Test2');
     });
-    this.tileMap = this.make.tilemap({ key: keys.json });
+    this.tileMap = this.make.tilemap({ key: 'mapJson3' });
     this.tileset = this.tileMap.addTilesetImage('map001', keys.image);
 
     // 各レイヤーを紐付ける(地面とか建物とか木とか...)
