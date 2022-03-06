0be1bb23b13c1a3e01f1380656f8154341e500ce
hk-p114514 <hukada.gogotea@gmail.com>
2022-03-06 22:32:19 +0900

    Playerの内容の共通部分をActorに追加し継承する形に変更

--- src/classes/Actor.ts
+++ src/classes/Actor.ts
@@ -1,12 +1,22 @@
-import { GameObjects, Scene, Tilemaps, Types } from 'phaser';
+import { GameObjects } from 'phaser';
+import { playerAnims } from 'playerAnims';
+import { tileSize } from 'scenes/Map.tpl';
+import { Direction } from './Direction';
 export type LimitValue = {
   current: number;
   max: number;
 };
 
+type Vector2 = Phaser.Math.Vector2;
+
 export type Level = {
+  // 現在のレベル
   current: number;
+  // 現在保持している経験値(レベルアップ時に0に戻す)
   exp: number;
+  // 次のレベルになるまでに必要な経験値の量
+  toNext: number;
+  // レベルの最大値
   max: number;
 };
 
@@ -33,15 +43,14 @@ export type SpriteAnims = {
 };
 
 export class Actor {
+  public offsetX: number = tileSize / 2;
+  public offsetY: number = tileSize;
   public name: string;
-  // private p: Point;
-  // private hp: LimitValue;
-  // private mp: LimitValue;
-  // private level: Level = { current: 1, exp: 0, max: 100 };
-  // private atk: number;
-  // private def: number;
-  // private diameter;
-  private sprite?: GameObjects.Sprite;
+  private hp: LimitValue;
+  private mp: LimitValue;
+  private level: Level = { current: 1, exp: 0, toNext: 1, max: 100 };
+  private atk: number;
+  private def: number;
   private animState: AnimState = 'front';
   private anims: SpriteAnims[] = [
     // 画像データは"ぴぽや"のキャラクターシートを使っているため、順番は固定
@@ -51,23 +60,72 @@ export class Actor {
     { key: 'right', start: 9, end: 11 },
   ];
   constructor(
-    spData: GameObjects.Sprite,
-    name: string,
-    // hp: LimitValue,
-    // mp: LimitValue,
-    // level: Level,
-    // atk: number,
-    // def: number,
-    // diameter: number,
+    public sprite: GameObjects.Sprite,
+    public tilePos: Vector2,
+    name: string = '',
+    hp: LimitValue = { current: 1, max: 1 },
+    mp: LimitValue = { current: 1, max: 1 },
+    level: Level = { current: 1, toNext: 1, exp: 1, max: 1 },
+    atk: number = 1,
+    def: number = 1,
   ) {
     this.name = name;
-    // const { x, y } = spData;
-    // this.p = { x, y };
-    // this.hp = hp;
-    // this.mp = mp;
-    // this.level = level;
-    // this.atk = atk;
-    // this.def = def;
-    // this.diameter = diameter;
+    this.hp = hp;
+    this.mp = mp;
+    this.level = level;
+    this.atk = atk;
+    this.def = def;
+
+    this.sprite.setOrigin(0.5, 1);
+    this.sprite.setPosition(
+      tilePos.x * tileSize + this.offsetX,
+      tilePos.y * tileSize + this.offsetY,
+    );
+    this.sprite.setFrame(playerAnims[playerAnims.length - 1].frameStart);
+  }
+
+  public beInjured(damage: number) {
+    this.hp.current - Math.floor(damage - damage / this.def);
+  }
+
+  getPosition(): Vector2 {
+    return this.sprite.getBottomCenter();
+  }
+
+  setPosition(position: Vector2): void {
+    this.sprite.setPosition(position.x, position.y);
+  }
+
+  stopAnimation(direction: Direction) {
+    const animationManager = this.sprite.anims.animationManager;
+    const standingFrame = animationManager.get(direction).frames[1].frame.name;
+    this.sprite.anims.stop();
+    this.sprite.setFrame(standingFrame);
+  }
+
+  startAnimation(direction: Direction) {
+    this.sprite.anims.play(direction);
+  }
+
+  getTilePos(): Vector2 {
+    return this.tilePos.clone();
+  }
+
+  // method overloading
+  setTilePos(tilePosition: Vector2): void;
+  setTilePos(x: number, y: number): void;
+  setTilePos(...values: any[]): void {
+    if (values.length === 1) {
+      this.tilePos = values[0].clone();
+    } else {
+      this.sprite.setPosition(
+        values[0] * tileSize + this.offsetX,
+        values[1] * tileSize + this.offsetY,
+      );
+    }
+  }
+
+  getSprite(): GameObjects.Sprite {
+    return this.sprite;
   }
 }
--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -1,64 +1,68 @@
-import { Scene, GameObjects } from 'phaser';
-import { Direction } from './Direction';
+import { GameObjects } from 'phaser';
 import { tileSize } from 'scenes/Map.tpl';
 import { playerAnims } from 'playerAnims';
+import { Actor, Level, LimitValue } from './Actor';
 
 type Vector2 = Phaser.Math.Vector2;
 
-export class Player {
+export class Player extends Actor {
   constructor(
-    /* parameter properties */
     public sprite: GameObjects.Sprite,
-    private tilePos: Vector2,
-    private offsetX: number = tileSize / 2,
-    private offsetY: number = tileSize,
+    tilePos: Vector2,
+    name: string = '',
+    hp: LimitValue = { current: 1, max: 1 },
+    mp: LimitValue = { current: 1, max: 1 },
+    level: Level = { current: 1, toNext: 1, exp: 1, max: 1 },
+    atk: number = 1,
+    def: number = 1,
   ) {
+    super(sprite, tilePos, name, hp, mp, level, atk, def);
     this.sprite.setOrigin(0.5, 1);
     this.sprite.setPosition(
-      tilePos.x * tileSize + this.offsetX,
-      tilePos.y * tileSize + this.offsetY,
+      this.tilePos.x * tileSize + this.offsetX,
+      this.tilePos.y * tileSize + this.offsetY,
     );
     this.sprite.setFrame(playerAnims[playerAnims.length - 1].frameStart);
   }
 
-  getPosition(): Vector2 {
-    return this.sprite.getBottomCenter();
-  }
+  // getPosition(): Vector2 {
+  //   return this.sprite.getBottomCenter();
+  // }
 
-  setPosition(position: Vector2): void {
-    this.sprite.setPosition(position.x, position.y);
-  }
+  // setPosition(position: Vector2): void {
+  //   this.sprite.setPosition(position.x, position.y);
+  // }
 
-  stopAnimation(direction: Direction) {
-    const animationManager = this.sprite.anims.animationManager;
-    const standingFrame = animationManager.get(direction).frames[1].frame.name;
-    this.sprite.anims.stop();
-    this.sprite.setFrame(standingFrame);
-  }
+  // stopAnimation(direction: Direction) {
+  //   const animationManager = this.sprite.anims.animationManager;
+  //   const standingFrame = animationManager.get(direction).frames[1].frame.name;
+  //   this.sprite.anims.stop();
+  //   this.sprite.setFrame(standingFrame);
+  // }
 
-  startAnimation(direction: Direction) {
-    this.sprite.anims.play(direction);
-  }
+  // startAnimation(direction: Direction) {
+  //   this.sprite.anims.play(direction);
+  // }
 
-  getTilePos(): Vector2 {
-    return this.tilePos.clone();
-  }
+  // getTilePos(): Vector2 {
+  //   return this.tilePos.clone();
+  // }
 
-  // method overloading
-  setTilePos(tilePosition: Vector2): void;
-  setTilePos(x: number, y: number): void;
-  setTilePos(...values: any[]): void {
-    if (values.length === 1) {
-      this.tilePos = values[0].clone();
-    } else {
-      this.sprite.setPosition(
-        values[0] * tileSize + this.offsetX,
-        values[1] * tileSize + this.offsetY,
-      );
-    }
-  }
+  // // method overloading
+  // setTilePos(tilePosition: Vector2): void;
+  // setTilePos(x: number, y: number): void;
+  // setTilePos(...values: any[]): void {
+  //   if (values.length === 1) {
+  //     this.tilePos = values[0].clone();
+  //   } else {
+  //     this.sprite.setPosition(
+  //       values[0] * tileSize + this.offsetX,
+  //       values[1] * tileSize + this.offsetY,
+  //     );
+  //   }
+  // }
 
-  getSprite(): GameObjects.Sprite {
-    return this.sprite;
-  }
+  // getSprite(): GameObjects.Sprite {
+  //   return this.sprite;
+  // }
 }
