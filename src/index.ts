81f09bc054e9d0b65ffc0b215c02c2b4799a0aa6
hk-p114514 <hukada.gogotea@gmail.com>
2022-03-08 01:15:36 +0900

    最初に上位クラスにプレイヤー、マップの情報を与える

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import { sceneKeys } from './scenes/sceneKeys';
 import { AUTO, Game, Types } from 'phaser';
-import playerSprite from '@/assets/characters/dynamic/player.png';
 import mapJson1 from '@/json/map001.json';
 import mapJson2 from '@/json/map002.json';
 import { H, W } from 'functions/DOM/windowInfo';
@@ -9,17 +7,17 @@ import { Map2 } from 'scenes/Map2';
 import { Preload } from './scenes/Preload';
 import '@/styles/index.scss';
 import { System } from 'classes/System';
+import { sceneKeys } from 'scenes/sceneKeys';
+import { players } from 'battleActors';
 
 // 使用するシーンをまとめる
 const scenes = [Preload, Map1, Map2];
 // const scenes = [Map1, Map2];
 
+export const system = new System(sceneKeys.map1, players[0]);
+
 // マップデータのjsonをまとめる
 export const json: string[] = [mapJson1, mapJson2];
-
-export const system = new System(playerSprite, sceneKeys.map1);
-
-console.log(system);
 
 class Main extends Game {
   constructor() {
