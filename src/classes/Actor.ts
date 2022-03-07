8b9494ba9e32a7171c60f8e21b21cdec99fa676f
hk-p114514 <hukada.gogotea@gmail.com>
2022-03-07 16:43:09 +0900

    Actor, Playerからステータス情報を削除

--- src/classes/Actor.ts
+++ src/classes/Actor.ts
@@ -2,37 +2,8 @@ import { GameObjects } from 'phaser';
 import { playerAnims } from 'playerAnims';
 import { tileSize } from 'scenes/Map.tpl';
 import { Direction } from './Direction';
-export type LimitValue = {
-  current: number;
-  max: number;
-};
 
 type Vector2 = Phaser.Math.Vector2;
-
-export type Level = {
-  // 現在のレベル
-  current: number;
-  // 現在保持している経験値(レベルアップ時に0に戻す)
-  exp: number;
-  // 次のレベルになるまでに必要な経験値の量
-  toNext: number;
-  // レベルの最大値
-  max: number;
-};
-
-export type Point = {
-  x: number;
-  y: number;
-};
-
-export type originActor = {
-  name: string;
-  hp: LimitValue;
-  mp: LimitValue;
-  level: Level;
-  atk: number;
-  def: number;
-};
 
 export type AnimState = 'front' | 'back' | 'left' | 'right' | '';
 
@@ -45,13 +16,6 @@ export type SpriteAnims = {
 export class Actor {
   public offsetX: number = tileSize / 2;
   public offsetY: number = tileSize;
-  public name: string;
-  private hp: LimitValue;
-  private mp: LimitValue;
-  private level: Level = { current: 1, exp: 0, toNext: 1, max: 100 };
-  private atk: number;
-  private def: number;
-  private animState: AnimState = 'front';
   private anims: SpriteAnims[] = [
     // 画像データは"ぴぽや"のキャラクターシートを使っているため、順番は固定
     { key: 'front', start: 0, end: 2 },
@@ -59,33 +23,13 @@ export class Actor {
     { key: 'left', start: 6, end: 8 },
     { key: 'right', start: 9, end: 11 },
   ];
-  constructor(
-    public sprite: GameObjects.Sprite,
-    public tilePos: Vector2,
-    name: string = '',
-    hp: LimitValue = { current: 1, max: 1 },
-    mp: LimitValue = { current: 1, max: 1 },
-    level: Level = { current: 1, toNext: 1, exp: 1, max: 1 },
-    atk: number = 1,
-    def: number = 1,
-  ) {
-    this.name = name;
-    this.hp = hp;
-    this.mp = mp;
-    this.level = level;
-    this.atk = atk;
-    this.def = def;
-
+  constructor(public sprite: GameObjects.Sprite, public tilePos: Vector2) {
     this.sprite.setOrigin(0.5, 1);
     this.sprite.setPosition(
       tilePos.x * tileSize + this.offsetX,
       tilePos.y * tileSize + this.offsetY,
     );
     this.sprite.setFrame(playerAnims[playerAnims.length - 1].frameStart);
-  }
-
-  public beInjured(damage: number) {
-    this.hp.current - Math.floor(damage - damage / this.def);
   }
 
   getPosition(): Vector2 {
--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -1,19 +1,10 @@
 import { GameObjects } from 'phaser';
-import { Actor, Level, LimitValue } from './Actor';
+import { Actor } from './Actor';
 
 type Vector2 = Phaser.Math.Vector2;
 
 export class Player extends Actor {
-  constructor(
-    public sprite: GameObjects.Sprite,
-    tilePos: Vector2,
-    name: string = '',
-    hp: LimitValue = { current: 1, max: 1 },
-    mp: LimitValue = { current: 1, max: 1 },
-    level: Level = { current: 1, toNext: 1, exp: 1, max: 1 },
-    atk: number = 1,
-    def: number = 1,
-  ) {
-    super(sprite, tilePos, name, hp, mp, level, atk, def);
+  constructor(public sprite: GameObjects.Sprite, tilePos: Vector2) {
+    super(sprite, tilePos);
   }
 }
