50613b27da7b8c233eccf306d0f8c21b63a19588
hk-p114514 <hukada.gogotea@gmail.com>
2022-03-24 11:59:07 +0900

    テスト用に技を追加

--- src/skills.ts
+++ src/skills.ts
@@ -3,19 +3,33 @@ import { Skill } from 'classes/Skill';
 
 export type SkillFunction = (attacker: BattleActor, targets: BattleActor[]) => void;
 
-const attackForAll: SkillFunction = (attacker: BattleActor, targets: BattleActor[]) => {
+const attackForAll = (attacker: BattleActor, targets: BattleActor[]) => {
   targets.forEach((target) => {
     target.beInjured(attacker.atk);
   });
 };
 
-const healForAll: SkillFunction = (attacker: BattleActor, targets: BattleActor[]) => {
+const healForAll = (
+  attacker: BattleActor,
+  targets: BattleActor[],
+  rate: number = 0.4,
+) => {
   targets.forEach((target) => {
-    target.beHealed(target.hp.max * 0.4);
+    target.beHealed(target.hp.max * rate);
+    console.log(
+      `${target.name}の体力は${target.hp.current} / ${target.hp.max}になった!!!`,
+    );
   });
+};
+
+const hightHealForAll = (attacker: BattleActor, targets: BattleActor[]) => {
+  healForAll(attacker, targets, 1);
 };
 
 export const skills: Skill[] = [
   new Skill('裏拳', attackForAll, false, true),
+  new Skill('日大災害誤射タックル', attackForAll, true, true),
   new Skill('ヒール', healForAll, false, false),
+  new Skill('日帰り旅行', healForAll, true, false),
+  new Skill('伊勢回転性', healForAll, true, false),
 ];
