e16acc457a4911b94bc95e84d337c1bfa6844754
hk-p114514 <hukada.gogotea@gmail.com>
2022-04-22 16:05:22 +0900

    logを削除

--- src/classes/TimelinePlayer.ts
+++ src/classes/TimelinePlayer.ts
@@ -42,7 +42,6 @@ export class TimelinePlayer extends Scene {
     this.backgroundLayer = this.add.container(0, 0);
     this.foregroundLayer = this.add.container(0, 0);
     this.uiLayer = this.add.container(0, 0);
-    console.log('init');
   }
 
   preload() {}
@@ -79,11 +78,9 @@ export class TimelinePlayer extends Scene {
 
     this.specTimeline({ timelineID: this.specID });
     this.timelineIndex = 0;
-    console.log('create');
   }
 
   update() {
-    console.log(1);
     if (!this.timeline) return;
     if (!this.dialogBox) return;
     if (!this.anotherScene) return;
@@ -94,7 +91,6 @@ export class TimelinePlayer extends Scene {
     if (!this.isTextShow) {
       return;
     }
-    console.log(2);
     // タイムラインのイベントを取得してから、timelineIndexをインクリメント
     const timelineEvent = this.timeline[this.timelineIndex++];
     console.log(this.anotherScene);
